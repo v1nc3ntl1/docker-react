09dca894efc5b83a08f2b62701d7d979b5a736cf
vincent li <v1nc3ntl1@gmail.com>
2021-05-16 17:45:02 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,11 +15,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          learn react
         </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
